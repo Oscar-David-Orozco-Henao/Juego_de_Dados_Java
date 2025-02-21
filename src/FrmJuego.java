797657c3b620986ed997d7129f9a22ef51d1a962
Oscar-David-Orozco-Henao <oscarorozco130397@correo.itm.edu.co>
2025-02-20 20:27:58 -0500

    Se completa interfaz gráfica de usuario

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 //extends es para Heredar
@@ -34,12 +39,42 @@ public class FrmJuego extends JFrame {
         JLabel lblTitulo1 = new JLabel("Lanzamientos");
         // Mostrar imagen (ubicación imagen - alto y ancho de la imagen)
         lblTitulo1.setBounds(50 + 2 * Imagen.getIconWidth(), 10, 100, 25);
-        getContentPane().add(lblTitulo1); // Show dado
+        lblTitulo1.setHorizontalAlignment(SwingConstants.CENTER);
+        getContentPane().add(lblTitulo1); // Show titulo
 
         JLabel lblTitulo2 = new JLabel("Cenas");
         // Mostrar imagen (ubicación imagen - alto y ancho de la imagen)
         lblTitulo2.setBounds(160 + 2 * Imagen.getIconWidth(), 10, 100, 25);
-        getContentPane().add(lblTitulo2); // Show dado
+        lblTitulo2.setHorizontalAlignment(SwingConstants.CENTER);
+        getContentPane().add(lblTitulo2); // Show titulo
+
+        JLabel lblLanzamientos = new JLabel();
+        lblLanzamientos.setBounds(50 + 2 * Imagen.getIconWidth(), 40, 100, 100);
+        lblLanzamientos.setBackground(new Color(0, 0, 0));
+        lblLanzamientos.setForeground(new Color(51, 255, 0));
+        lblLanzamientos.setFont(new Font("Tahoma", 1, 72));
+        lblLanzamientos.setText("0");
+        lblLanzamientos.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblLanzamientos.setOpaque(true);
+        getContentPane().add(lblLanzamientos);
+
+        JLabel lblCenas = new JLabel();
+        lblCenas.setBounds(160 + 2 * Imagen.getIconWidth(), 40, 100, 100);
+        lblCenas.setBackground(new Color(0, 0, 0));
+        lblCenas.setForeground(new Color(51, 255, 0));
+        lblCenas.setFont(new Font("Tahoma", 1, 72));
+        lblCenas.setText("0");
+        lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblCenas.setOpaque(true);
+        getContentPane().add(lblCenas);
+
+        JButton btnIniciar = new JButton("iniciar");
+        btnIniciar.setBounds(10, 15 + Imagen.getIconHeight(), 100, 25);
+        getContentPane().add(btnIniciar);
+
+        JButton btnLanzar = new JButton("Lanzar");
+        btnLanzar.setBounds(10, 15 + Imagen.getIconHeight(), 100, 25);
+        getContentPane().add(btnLanzar);
 
     }
 }
